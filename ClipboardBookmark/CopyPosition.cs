1330eafd8a2873a339213ffb50ce696cddd70cee
Kovalenko Andriy <a.kovalenko.ua@gmail.com>
2017-10-30 23:45:16 +0200

    First commit

---
+++ ClipboardBookmark/CopyPosition.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System.Diagnostics;
+
+namespace ClipboardBookmark
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CopyPosition
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CopyCommandId = 0x0100;
+        public const int PasteCommandId = 0x0200;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("ba59338a-1034-4028-a911-e93e59516768");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyPosition"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private CopyPosition(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCopyCommandID = new CommandID(CommandSet, CopyCommandId);
+                var menuItemCopy = new MenuCommand(this.MenuItemCopyCallback, menuCopyCommandID);
+                commandService.AddCommand(menuItemCopy);
+
+                var menuPasteCommandID = new CommandID(CommandSet, PasteCommandId);
+                var menuItemPaste = new MenuCommand(this.MenuItemPasteCallback, menuPasteCommandID);
+                commandService.AddCommand(menuItemPaste);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CopyPosition Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new CopyPosition(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCopyCallback(object sender, EventArgs e)
+        {
+            Document activeDoc = ((CopyPositionPackage)package).dte.ActiveDocument;
+
+            if (activeDoc != null)
+            {
+                TextDocument textDoc = (TextDocument)activeDoc.Object("TextDocument");
+                if (textDoc != null)
+                {
+                    string fullName = activeDoc.FullName;
+                    var bottomLine = textDoc.Selection.BottomLine;
+                    //var topLine = textDoc.Selection.TopLine;
+                    System.Windows.Forms.Clipboard.SetText(fullName + "|" + bottomLine + "\r\n");
+                }
+            }
+        }
+
+        private void MenuItemPasteCallback(object sender, EventArgs e)
+        {
+            string message = System.Windows.Forms.Clipboard.GetText();
+            bool positionExtracted = false;
+            if (message.Contains("|") && message.Length < 500)
+            {
+                var spl = message.Split('|');
+                if (spl.Length <= 2 && spl.Length > 0)
+                {
+                    string fileName = spl[0];
+                    string line = spl.Length == 1 ? "0" : spl[1];
+                    positionExtracted = true;
+                    OpenFileAndGotoLine(fileName, line);
+                }
+            }
+            if (!positionExtracted)
+            {
+                string title = "Paste Position";
+
+                message = "Position could not be extracted from clipboard.";
+                VsShellUtilities.ShowMessageBox(
+                    this.ServiceProvider,
+                    message,
+                    title,
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+
+        }
+
+        private void OpenFileAndGotoLine(string fileName, string stringLine)
+        {
+            IVsUIShellOpenDocument openDoc = Package.GetGlobalService(typeof(IVsUIShellOpenDocument)) as IVsUIShellOpenDocument;
+            IVsWindowFrame frame;
+            Microsoft.VisualStudio.OLE.Interop.IServiceProvider sp;
+            IVsUIHierarchy hier;
+            uint itemid;
+            Guid logicalView = VSConstants.LOGVIEWID_Code;
+            if (ErrorHandler.Failed(openDoc.OpenDocumentViaProject(fileName, ref logicalView, out sp, out hier, out itemid, out frame))
+                  || frame == null)
+            {
+                return;
+            }
+            object docData;
+            frame.GetProperty((int)__VSFPROPID.VSFPROPID_DocData, out docData);
+            VsTextBuffer buffer = docData as VsTextBuffer;
+            if (buffer == null)
+            {
+                IVsTextBufferProvider bufferProvider = docData as
+                  IVsTextBufferProvider;
+                if (bufferProvider != null)
+                {
+                    IVsTextLines lines;
+                    ErrorHandler.ThrowOnFailure(bufferProvider.GetTextBuffer(out lines));
+                    buffer = lines as VsTextBuffer;
+                    Debug.Assert(buffer != null, "IVsTextLines does not implement IVsTextBuffer");
+                    if (buffer == null)
+                    {
+                        return;
+                    }
+                }
+            }
+            IVsTextManager mgr = Package.GetGlobalService(typeof(VsTextManagerClass)) as IVsTextManager;
+            int line = 0;
+            Int32.TryParse(stringLine, out line);
+            mgr.NavigateToLineAndColumn(buffer, ref logicalView, line, 0, line, 0);
+        }
+    }
+}
